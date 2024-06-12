a44822e1b188dc13d14695faf94152a6b9f671c0
lacithelaci <68651344+lacithelaci@users.noreply.github.com>
2024-06-12 20:55:01 +0200

    Update akasztofa.py

--- egyetemi_cuccok/akasztofa.py
+++ egyetemi_cuccok/akasztofa.py
@@ -1,63 +1,49 @@
 import random
 
 
-def benne_van(kitalalando_szo: str, betu: str):
-    ures = ""
-    for i in range(0, len(kitalalando_szo)):
-        if kitalalando_szo[i] == betu:
-            ures += betu
-        else:
-            ures += "_"
-    return ures
+def benne_van(betu, kivalasztott_szo) -> bool:
+    return betu in kivalasztott_szo
 
 
-def atiro(jelenlegi_allas, betukeres_utani_allas):
-    ures = ""
-    for i in range(0, len(jelenlegi_allas)):
-        if jelenlegi_allas[i] != "_":
-            ures += jelenlegi_allas[i]
-        elif jelenlegi_allas[i] == "_" and betukeres_utani_allas[i] != "_":
-            ures += betukeres_utani_allas[i]
-        else:
-            ures += "_"
-    return ures
+def kirajzolo(kirajzolt, kivalasztott_szo, betu) -> str:
+    return ''.join(k if k == b or k == betu else '-' for k, b in zip(kivalasztott_szo, kirajzolt))
 
 
-lista = []
-f = open("szavak.txt", encoding="UTF-8")
-for i in f:
-    i = i.strip()
-    lista.append(i)
+def beolvas() -> list[str]:
+    with open("szavak.txt", encoding="utf-8") as f:
+        lista = [i.strip() for i in f]
+    return lista
 
-db = 1
-kitalalando_szo = random.choice(lista)
-allas = "_" * len(kitalalando_szo)
-eddig_használt_betűk = []
-print("A szó hossza: ", len(kitalalando_szo))
-while True:
-    print("Jelenlegi próbálkozás:", db)
-    bekert_betu = input("Kérek egy betűt: ")
-    if len(bekert_betu) == 1:
-        minta = benne_van(kitalalando_szo, bekert_betu)
-        allas = atiro(minta, allas)
-        db += 1
-        print(allas)
-    elif len(bekert_betu) == len(kitalalando_szo) and bekert_betu == kitalalando_szo:
-        print("Nyertél, Ennyi próbálkozásból:", db)
-        break
-    elif len(bekert_betu) == len(kitalalando_szo) and bekert_betu != kitalalando_szo:
-        print("Vesztettél")
-        print("Ez a szó volt", kitalalando_szo)
-        break
-    else:
-        print("Hiba, nem karaktert adtál meg! Büntetés: -1 kitalálás")
-        db += 1
-    if kitalalando_szo == allas:
-        print("Nyertél")
-        break
-    if db == len(kitalalando_szo) + 1:
-        print("Vesztettél!")
-        print("Ez a szó volt", kitalalando_szo)
-        break
-    eddig_használt_betűk.append(bekert_betu)
-    print("Eddig felhasznált betűk:", *eddig_használt_betűk)
+
+import random
+
+
+def main() -> None:
+    talalat = 1
+    kivalasztott_szo = random.choice(beolvas())
+    kirajzolt = '_' * len(kivalasztott_szo)
+    print(f"A kitalálandó szó {len(kivalasztott_szo)} karakter hosszú: {kirajzolt}\n")
+
+    while True:
+        betu = input()
+        if len(betu) > 1:
+            if betu == kivalasztott_szo:
+                print(f"Ügyes vagy, kitaláltad {talalat} próbálkozásból!\n")
+            else:
+                print(f"Sajnálom, vesztettél\nA kitalálandó szó nem volt más mint: {kivalasztott_szo}\n")
+            break
+
+        if benne_van(betu, kivalasztott_szo):
+            kirajzolt = kirajzolo(kirajzolt, kivalasztott_szo, betu)
+
+        print(kirajzolt + '\n')
+
+        if kirajzolt == kivalasztott_szo:
+            print(f"Ügyes vagy, kitaláltad {talalat} próbálkozásból!\n")
+            break
+
+        talalat += 1
+
+
+if __name__ == '__main__':
+    main()
