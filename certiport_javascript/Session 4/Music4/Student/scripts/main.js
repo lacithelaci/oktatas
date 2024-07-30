e34acebb5303b920bac4deb2fdcfa7ddca6883ff
lacithelaci <68651344+lacithelaci@users.noreply.github.com>
2024-07-30 09:59:33 +0200

    Add files via upload

---
+++ certiport_javascript/Session 4/Music4/Student/scripts/main.js
@@ -0,0 +1,8 @@
+﻿const salesTaxRate = .075;
+incMsg = "This site is under construction and will be fully functional soon.";
+function product(item, description, price, imgsrc) {
+    this.item = item;
+    this.description = description;
+    this.price = price;
+    this.imgsrc = imgsrc;
+}
