c5eae207024b92a48fe855a24874a4c1ae100205
lacithelaci <68651344+lacithelaci@users.noreply.github.com>
2025-09-23 13:23:14 +0200

    Update app.py

--- egyetemi_feleves_cuccok/5_szemeszter/adatkezelo/ora2/data/app.py
+++ egyetemi_feleves_cuccok/5_szemeszter/adatkezelo/ora2/data/app.py
@@ -1,5 +1,35 @@
-from data.basic.model_classes import Person
+from http.client import CannotSendRequest
 
-tom = Person('P01','Tom',20,True)
+from ora2.data.basic.model_dataclasses import Person, Car, Airport
+import pandas as pd
 
-print(tom)
+p1 = Person("001", "Name-001", 12,True)
+p2 = Person("002", "Name-002", 14,True)
+p3 = Person("003", "Name-003", 16,False)
+p4_not_in_list = Person("004", "Name-004", 18, False)
+
+person_list = [p1, p2, p3]
+
+
+df1 = pd.DataFrame.from_records([p.__dict__ for p in person_list])
+print(df1)
+
+print()
+
+r1 = Car('s','w',1999,True)
+r2 = Car('s','w',1999,True)
+r3 = Car('s','w',1999,True)
+r4 = Car('s','w',1999,True)
+
+car_list  = [r1, r2, r3]
+df2 = pd.DataFrame.from_records([p.__dict__ for p in car_list])
+print(df2)
+
+
+r1 = Airport('a','b','c','d','e')
+r2 = Airport('a','b','c','d','e')
+r3 = Airport('a','b','c','d','e')
+print()
+airport_list  = [r1, r2, r3]
+df3 = pd.DataFrame.from_records([p.__dict__ for p in airport_list])
+print(df3)
